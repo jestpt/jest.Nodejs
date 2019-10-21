cf9e0d10e05e8af17871f48da1be44320add689d
Bruno Grifo <leitaogrifo@gmail.com>
2019-10-21 01:11:14 +0100

    first commit. App running

---
+++ server.js
@@ -0,0 +1,18 @@
+//express
+//require dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 3000;
+
+var router = require('./app/routes');
+app.use('/',router);
+
+//set static files (css,images,etc) location
+app.use(express.static(__dirname + '/public'));
+
+//start server
+app.listen(port, function(){
+    console.log('app started');
+});
+

e0d6a6a4c636928728293fbdfc727f84c997c192
Bruno Grifo <leitaogrifo@gmail.com>
2019-10-30 22:37:01 +0000

    all fonts done

--- server.js
+++ server.js
@@ -3,12 +3,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 //const favicon = require('express-favicon');
-var favicon = require('serve-favicon');
 var app = express();
 var port = process.env.PORT || 3000;
-
-app.use(favicon((__dirname, 'public/jest.ico')));
-
 
 var router = require('./app/routes');
 app.use('/',router);
